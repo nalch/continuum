f56845fd2933a86e5ba6b14ab8e904dedbfa82bc
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-08 22:25:29 +0200

    codestyle

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,17 +3,20 @@ var utils = require('../utils');
 
 /**
  * generate user id, if it is not already in the session
+ * @param {Request} req the request
+ * @param {Response} res the response
+ * @param {func} next finished callback
  */
-exports.generateUserId = function (req, res, next) {
+exports.generateUserId = function(req, res, next) {
   if (!req.session.userId) {
-	  req.session.userId = utils.guid();
-	  Player.create(
-	    {
-		  publicId: req.session.userId
-		}, function(err, player) {
-			// todo for a later time: http://expressjs.com/de/guide/error-handling.html
-	    }
-	  );
+    req.session.userId = utils.guid();
+    Player.create({
+      publicId: req.session.userId
+    }, function(err) {
+      if (err) {
+        // todo for a later time: http://expressjs.com/de/guide/error-handling.html
+      }
+    });
   }
   next();
 };
