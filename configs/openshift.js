7e80592873d8e0897fd1e4b939ab4cdded7b5ecd
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-14 01:00:28 +0200

    lets add web object to settings

--- configs/openshift.js
+++ configs/openshift.js
@@ -3,8 +3,10 @@ var config = {};
 config.session = {};
 config.session.secretKey = process.env.OPENSHIFT_SECRET_TOKEN;
 
-config.web.ip = process.env.OPENSHIFT_NODEJS_IP;
-config.web.port = process.env.OPENSHIFT_NODEJS_PORT;
+config.web = {
+  ip: process.env.OPENSHIFT_NODEJS_IP,
+  port: process.env.OPENSHIFT_NODEJS_PORT
+}
 
 config.db = {
   host: process.env.OPENSHIFT_MONGODB_DB_HOST,
