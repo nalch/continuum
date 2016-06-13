eb1600509e4406217b304decda8b3a7402f211d2
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-13 23:10:21 +0200

    add deployment settings for openshift

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 var extend = require('extend');
 
-var stageConfig = require('./configs/' + (process.env.STAGE || 'development'));
+var stage = process.env.OPENSHIFT_APP_NAME ? 'openshift' : process.env.STAGE || 'development';
+var stageConfig = require('./configs/' + stage);
 
 // setting defaults
 var config = {
-  stage: process.env.STAGE || 'development',
+  stage: stage,
   testing: process.env.TESTING || false,
   session: {
     secretKey: ''
   },
   web: {
-    port: process.env.WEB_PORT || 8300
+    port: process.env.WEB_PORT || 8080
   }
 };
 
