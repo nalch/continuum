e39ca142b8e8e5896f3be5367f2d7211b63ddd72
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-14 21:24:51 +0200

    add periodical games update, closes #16

--- public/javascripts/controllers/startController.js
+++ public/javascripts/controllers/startController.js
@@ -6,21 +6,47 @@ var startController = angular.module('startController', []);
 
 startController.controller(
   'startController',
-  function ($controller, $scope, $http, $filter, $location, GameService, initialGames) {
+  function ($controller, $scope, $http, $interval, $filter, $location, GameService, initialGames) {
     $controller('errorController', {$scope: $scope});
     $scope.formData = {};
-    $scope.games = $filter('orderBy')(
-      $filter('filter')(
-        initialGames,
-        function(value, index, array) {
-          return value.state !== 2;
-        },
-        true
-      ),
-      function(game) {
-        return game.state;
-      }
-    );
+
+    $scope.filterGames = function(games) {
+      return $filter('orderBy')(
+        $filter('filter')(
+          games,
+          function(value, index, array) {
+            return value.state !== 2;
+          },
+          true
+        ),
+        function(game) {
+          return game.state;
+        }
+      );
+    }
+
+    $scope.updateView = function() {
+      GameService.query(function(games) {
+        $scope.games = $scope.filterGames(games);
+        console.log($scope.games);
+      }, function(error) {
+        $scope.addError('Could not get games');
+      });
+    };
+
+    $scope.startViewUpdate = function() {
+      $scope.updateView();
+      $scope.heartbeat = $interval($scope.updateView, 1000);
+    };
+
+    $scope.stopViewUpdate = function() {
+        $interval.cancel($scope.heartbeat);
+    };
+
+    $scope.startViewUpdate();
+    $scope.$on('$destroy', function() {
+        $scope.stopViewUpdate();
+    });
 
     $scope.createGame = function() {
       data = {}
@@ -43,5 +69,7 @@ startController.controller(
         });
       }
     };
+
+    $scope.games = $scope.filterGames(initialGames);
   }
 );
