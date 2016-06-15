36935c3e096fb1ce20c2d6b78f0c362c8d2655ba
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-15 12:08:31 +0200

    introduced class ordering in controllers #7

--- public/static/javascripts/controllers/StartController.js
+++ public/static/javascripts/controllers/StartController.js
@@ -1,15 +1,33 @@
 angular.module('StartController', []).controller(
   'StartController',
   function($controller, $scope, $interval, $filter, $location, GameService, initialGames) {
+    // controller initialisation
     var vm = this;
     $controller('ErrorController', {vm: vm});
     vm.formData = {};
 
-    vm.filterGames = function(games) {
+    // available functions
+    vm.filterGames = filterGames;
+    vm.updateView = updateView;
+    vm.startViewUpdate = startViewUpdate;
+    vm.stopViewUpdate = stopViewUpdate;
+
+    vm.createGame = createGame;
+    vm.joinGame = joinGame;
+
+    // controller start
+    vm.games = vm.filterGames(initialGames);
+    vm.startViewUpdate();
+    $scope.$on('$destroy', function() {
+      vm.stopViewUpdate();
+    });
+
+    // function implementations
+    function filterGames(games) {
       return $filter('orderBy')(
         $filter('filter')(
           games,
-          function(value, index, array) {
+          function(value) {
             return value.state !== 2;
           },
           true
@@ -20,30 +38,25 @@ angular.module('StartController', []).controller(
       );
     }
 
-    vm.updateView = function() {
+    function updateView() {
       GameService.query(function(games) {
         vm.games = vm.filterGames(games);
-      }, function(error) {
+      }, function() {
         vm.addError('Could not get games');
       });
-    };
+    }
 
-    vm.startViewUpdate = function() {
+    function startViewUpdate() {
       vm.updateView();
       vm.heartbeat = $interval(vm.updateView, 1000);
-    };
+    }
 
-    vm.stopViewUpdate = function() {
+    function stopViewUpdate() {
       $interval.cancel(vm.heartbeat);
-    };
+    }
 
-    vm.startViewUpdate();
-    $scope.$on('$destroy', function() {
-      vm.stopViewUpdate();
-    });
-
-    vm.createGame = function() {
-      data = {}
+    function createGame() {
+      var data = {};
       if (vm.gameformData) {
         data = vm.gameformData;
       }
@@ -52,20 +65,18 @@ angular.module('StartController', []).controller(
       }, function(error) {
         vm.addError(error);
       });
-    };
+    }
 
-    vm.joinGame = function() {
+    function joinGame() {
       if (vm.gameformData) {
         GameService.get({gameId: vm.gameformData.publicId}, function(game) {
           $location.path('/lobby/' + game.publicId);
-        }, function(error) {
+        }, function() {
           vm.addError('Game does not exist');
         });
       } else {
         vm.addError('Game id is empty');
       }
-    };
-
-    vm.updateView();
+    }
   }
 );
