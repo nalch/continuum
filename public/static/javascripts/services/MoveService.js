a8570e7594797ea67623362d005eac8a6dbc6900
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-15 14:43:04 +0200

    moveservice bugfix #7

--- public/static/javascripts/services/MoveService.js
+++ public/static/javascripts/services/MoveService.js
@@ -1,3 +1,3 @@
-angular.module('services', ['ngResource']).factory('MoveService', ['$resource', function($resource) {
+angular.module('services').factory('MoveService', ['$resource', function($resource) {
   return $resource('/games/:gameId/moves/:moveId');
 }]);
