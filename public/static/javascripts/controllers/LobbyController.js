48a404761dc0c4314b4ae27d7ce567a67a05c5bf
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-15 13:05:54 +0200

    bring LobbyController up-to-date

--- public/static/javascripts/controllers/LobbyController.js
+++ public/static/javascripts/controllers/LobbyController.js
@@ -1,53 +1,53 @@
-angular.module('LobbyController', []).controller(
-  'LobbyController',
-  function ($controller, $scope, $http, $routeParams, $location, $interval, GameService, initialGame) {
-    // controller initialisation
-    var vm = this;
-    $controller('GameUpdateController', {vm: vm});
-    vm.game = initialGame;
-	  vm.location = $location;
+angular.module('LobbyController', []).controller('LobbyController', LobbyController);
 
-    // available functions
-    vm.isOwner = isOwner;
-    vm.setNick = setNick;
-    vm.challengeUser = challengeUser;
-    vm.startGame = startGame;
+function LobbyController($controller, $scope, $http, $routeParams, $location, $interval, GameService, initialGame) {
+  // controller initialisation
+  var vm = this;
+  $controller('GameUpdateController', {vm: vm});
+  vm.game = initialGame;
+  vm.location = $location;
 
-    // controller start
+  // available functions
+  vm.isOwner = isOwner;
+  vm.setNick = setNick;
+  vm.challengeUser = challengeUser;
+  vm.startGame = startGame;
 
-    // function implementations
-    function isOwner() {
-      return $scope.userId === $scope.game.owner.publicId;
-    };
+  // controller start
 
-    function setNick(data) {
-      $http.put('/players/' + $scope.userId, {'nick': data})
-        .success(function (data) {
-          return true;
-        })
-        .error(function (data) {
-          return 'Could not change nick';
-        });
-    };
-
-    function challengeUser(visitor) {
-      $http.put('/games/' + $routeParams.gameId, {opponentId: visitor.publicId})
-      .success(function (data) {
-        $scope.game.opponent = visitor;
-      })
-      .error(function (data) {
-        console.log('Error: ' + data);
-      });
-    };
-
-    function startGame() {
-      GameService.get({gameId: $routeParams.gameId}, function(game) {
-        if (game.state !== 2) {
-          $location.path('/continuum/' + $routeParams.gameId);
-        } else {
-          $scope.addError('Game is finished');
-        }
-      });
-    };
+  // function implementations
+  function isOwner() {
+    return $scope.userId === $scope.game.owner.publicId;
   }
-);
+
+  function setNick(data) {
+    $http.put('/players/' + $scope.userId, {nick: data})
+      .success(function() {
+        return true;
+      })
+      .error(function() {
+        vm.addError('Could not change nick');
+        return 'Could not change nick';
+      });
+  }
+
+  function challengeUser(visitor) {
+    $http.put('/games/' + $routeParams.gameId, {opponentId: visitor.publicId})
+    .success(function() {
+      $scope.game.opponent = visitor;
+    })
+    .error(function() {
+      vm.addError('Could not challenge user');
+    });
+  }
+
+  function startGame() {
+    GameService.get({gameId: $routeParams.gameId}, function(game) {
+      if (game.state === enumvalues.GameState.FINISHED) {
+        vm.addError('Game is finished');
+      } else {
+        $location.path('/continuum/' + $routeParams.gameId);
+      }
+    });
+  }
+}
