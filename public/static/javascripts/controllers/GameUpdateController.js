d7e049de0a91f418fc8521893532ac5ec6cd31d2
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-07-27 21:28:16 +0200

    gameupdatecontroller jsdoc

--- public/static/javascripts/controllers/GameUpdateController.js
+++ public/static/javascripts/controllers/GameUpdateController.js
@@ -18,6 +18,10 @@ angular.module('GameUpdateController', []).controller(
       });
 
       // function implementations
+
+      /**
+       * set the updatingView viewmodel to true, requests the game and sets the updatingView var back to false
+       */
       function updateView() {
         vm.updatingView = true;
         GameService.get({gameId: $routeParams.gameId}, function(game) {
@@ -30,11 +34,17 @@ angular.module('GameUpdateController', []).controller(
         });
       }
 
+      /**
+       * starts the view update (@see updateView) and schedule its every three seconds
+       */
       function startViewUpdate() {
         vm.updateView();
         vm.heartbeat = $interval(vm.updateView, 3000);
       }
 
+      /**
+       * cancels the view update (@see updateView)
+       */
       function stopViewUpdate() {
         $interval.cancel(vm.heartbeat);
       }
